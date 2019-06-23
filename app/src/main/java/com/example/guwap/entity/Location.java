10f73b0a3323e4e773085ba021e0ad8e96b98bf1
William Kang <williamkang@Williams-MBP-3.attlocal.net>
2019-06-23 10:09:14 -0400

    more recent changes

--- app/src/main/java/com/example/guwap/entity/Location.java
+++ app/src/main/java/com/example/guwap/entity/Location.java
@@ -1,4 +1,41 @@
 package com.example.guwap.entity;
 
 public class Location {
+    private double lattitude;
+    private double longitude;
+    private String name;
+
+    public void setLattitude(double lattitude) {
+        this.lattitude = lattitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location(double lattitude, double longitude, String name) {
+        this.lattitude = lattitude;
+        this.longitude = longitude;
+        this.name = name;
+    }
+
+    public Location(String name) {
+        this(Math.random()*(33.7493-33.7487)- 33.7493, Math.random()*(84.3877 - 84.3883) - 84.3877, name)
+    }
+
+    public double getLattitude() {
+        return lattitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- app/src/main/java/com/example/guwap/entity/Universe.java
+++ app/src/main/java/com/example/guwap/entity/Universe.java
@@ -1,58 +1,25 @@
 package com.example.mapsdemo;
 
-import android.location.Location;
+import com.example.guwap.entity.Location;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Locations {
+public class Universe{
 
-    public static List<Locations.Location> locationArrayList = Arrays.asList(
-
+    public static List<Location> locationArrayList = Arrays.asList(
+            new Location("Ancilife city"),
+            new Location("Raditown"),
+            new Location("Creatown"),
+            new Location("Buzzcity"),
+            new Location("What's The Word Town"),
+            new Location("I don't know town"),
+            new Location("Hi town"),
+            new Location("What are some good townnames"),
+            new Location("I'm not creative"),
+            new Location("help")
     );
-
-
-
-    public static class Location {
-        private double lattitude;
-        private double longitude;
-        private String name;
-
-        public void setLattitude(double lattitude) {
-            this.lattitude = lattitude;
-        }
-
-        public void setLongitude(double longitude) {
-            this.longitude = longitude;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Location(double lattitude, double longitude, String name) {
-            this.lattitude = lattitude;
-            this.longitude = longitude;
-            this.name = name;
-        }
-
-        public Location(String name) {
-            this(Math.random()*(33.7493-33.7487)- 33.7493, Math.random()*(84.3877 - 84.3883) - 84.3877, name)
-        }
-
-        public double getLattitude() {
-            return lattitude;
-        }
-
-        public double getLongitude() {
-            return longitude;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
 
 
 }
