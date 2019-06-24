e9b4f525b0e39a2ff386d25e2dc52c4d56199564
Muddassir Bahri <MuddassirBahri@Muddassirs-MacBook-Pro.local>
2019-06-23 23:23:41 -0400

    Updated Location and Universe

--- app/src/main/java/com/example/guwap/entity/Location.java
+++ app/src/main/java/com/example/guwap/entity/Location.java
@@ -1,9 +1,15 @@
 package com.example.guwap.entity;
+import android.provider.Contacts;
+
+import java.util.Random;
 
 public class Location {
     private double lattitude;
     private double longitude;
     private String name;
+    private PeopleType peopleType;
+    private Resources resources;
+
 
     public void setLattitude(double lattitude) {
         this.lattitude = lattitude;
@@ -17,14 +23,20 @@ public class Location {
         this.name = name;
     }
 
+    public Location(String name) {
+        this(Math.random()*(33.7493-33.7487)- 33.7493, Math.random()*(84.3877 - 84.3883) - 84.3877, name, PeopleType.randPeopleType(), Resources.randResources());
+    }
+
     public Location(double lattitude, double longitude, String name) {
+        this(lattitude, longitude, name, PeopleType.randPeopleType(), Resources.randResources());
+    }
+
+    public Location(double lattitude, double longitude, String name, PeopleType peopleType, Resources resources) {
         this.lattitude = lattitude;
         this.longitude = longitude;
         this.name = name;
-    }
-
-    public Location(String name) {
-        this(Math.random()*(33.7493-33.7487)- 33.7493, Math.random()*(84.3877 - 84.3883) - 84.3877, name);
+        this.peopleType = peopleType;
+        this.resources = resources;
     }
 
     public double getLattitude() {
@@ -38,4 +50,5 @@ public class Location {
     public String getName() {
         return name;
     }
+
 }
--- app/src/main/java/com/example/guwap/entity/Universe.java
+++ app/src/main/java/com/example/guwap/entity/Universe.java
@@ -9,21 +9,20 @@ import java.util.List;
 public class Universe{
 
     public static List<Location> locationArrayList = Arrays.asList(
-            new Location("Dodge City"),
-            new Location("Tombstone"),
-            new Location("Virginia City"),
-            new Location("Oatman"),
-            new Location("Cody"),
-            new Location("Deadwood"),
-            new Location("Durango"),
-            new Location("Silverton"),
-            new Location("Bandera"),
-            new Location("Amarillo"),
-            new Location("Santa Fe"),
-            new Location("Cripple Creek"),
-            new Location("Buffalo"),
-            new Location("Abilene")
+            new Location(15, 16,"Dodge City"),
+            new Location(22, 19,"Tombstone"),
+            new Location(43, 32, "Virginia City"),
+            new Location(13, 20,"Oatman"),
+            new Location(14,22,"Cody"),
+            new Location(50, 16, "Deadwood"),
+            new Location(41, 50, "Durango"),
+            new Location(100, 55, "Silverton"),
+            new Location(94, 22, "Bandera"),
+            new Location(25, 15, "Amarillo"),
+            new Location(39, 39, "Santa Fe"),
+            new Location(22, 22,"Cripple Creek"),
+            new Location(73, 73, "Buffalo"),
+            new Location(36, 36,"Abilene")
     );
-
 
 }
