4d064d4e75109580a2e564cf261c96a5a8b041d7
Gabrielle Schweinfurth <gabi.schweinfurth@gmail.com>
2019-07-18 16:28:36 -0400

    XMLs for ConflictResolved, EncounterActivity, GamerOver, LoadActivity, and TitleScreen.

--- app/src/main/java/com/example/guwap/view/GameOver.java
+++ app/src/main/java/com/example/guwap/view/GameOver.java
@@ -2,6 +2,8 @@ package com.example.guwap.view;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
@@ -14,7 +16,13 @@ public class GameOver extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        titleScreen = findViewById(R.id.backButton);
+        titleScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTitleScreenPressed();
+            }
+        });
     }
 
     protected void onTitleScreenPressed() {
