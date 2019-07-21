9082d166648b121be4aacefe5735669b5d5d2826
Gabrielle Schweinfurth <gabi.schweinfurth@gmail.com>
2019-07-21 12:59:08 -0400

    Junit

--- app/src/main/java/com/example/guwap/view/LoadActivity.java
+++ app/src/main/java/com/example/guwap/view/LoadActivity.java
@@ -31,10 +31,10 @@ public class LoadActivity extends AppCompatActivity {
         //        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //        difficulty.setAdapter(adapter);
 
-        ArrayAdapter<Player> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, );
+        //ArrayAdapter<Player> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, );
         //Name, Difficulty, Current Region??? possible characteristics in spinner
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        characterOptions.setAdapter(adapter);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //characterOptions.setAdapter(adapter);
 
         load = findViewById(R.id.LoadButton);
         load.setOnClickListener(new View.OnClickListener() {
